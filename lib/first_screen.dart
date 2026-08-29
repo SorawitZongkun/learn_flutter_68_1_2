86aa1922a10cc58db1ee83371eddfda0543d66bd
Sorawit <thankzaki@hotmail.com>
2026-08-29 13:38:18 +0700

    Step 3: Check internet connection, Step 4: Show toast message

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'dart:async';
 
+// Step 3: Check internet connection
+import 'package:connectivity_plus/connectivity_plus.dart';
+// Step 4: Show toast message
+import 'package:fluttertoast/fluttertoast.dart';
+
 class FirstScreen extends StatefulWidget {
   const FirstScreen({super.key});
 
@@ -16,14 +21,53 @@ class _FirstScreenState extends State<FirstScreen> {
   void initState() {
     super.initState();
 
-    // เมื่อครบ 3 วิ ให้ไปหน้า Second Screen
-    Timer(
-      const Duration(seconds: 3),
-      () => Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => const SecondScreen()),
-      ),
-    );
+    // Step 3: Check internet connection
+    checkInternetConnection();
+  }
+
+  void checkInternetConnection() async {
+    final List<ConnectivityResult> connectivityResult = await (Connectivity()
+        .checkConnectivity());
+
+    // Determines whether any active network connection exists.
+    if (connectivityResult.hasConnectivity) {
+      // Connectivity available (regardless of the underlying transport).
+    }
+
+    // This condition is for demo purposes only to explain every connection type.
+    // Use conditions which work for your requirements.
+    if (connectivityResult.contains(ConnectivityResult.mobile)) {
+      // Mobile network available.
+      _showToast(context, "Mobile network available.");
+    } else if (connectivityResult.contains(ConnectivityResult.wifi)) {
+      // Wi-fi is available.
+      // Note for Android:
+      // When both mobile and Wi-Fi are turned on system will return Wi-Fi only as active network type
+      _showToast(context, "Wi-fi is available.");
+    } else if (connectivityResult.contains(ConnectivityResult.ethernet)) {
+      // Ethernet connection available.
+      _showToast(context, "Ethernet connection available.");
+    } else if (connectivityResult.contains(ConnectivityResult.vpn)) {
+      // Vpn connection active.
+      // Note for iOS and macOS:
+      // There is no separate network interface type for [vpn].
+      // It returns [other] on any device (also simulator)
+      _showToast(context, "Vpn connection active.");
+    } else if (connectivityResult.contains(ConnectivityResult.bluetooth)) {
+      // Bluetooth connection available.
+      _showToast(context, "Bluetooth connection available.");
+    } else if (connectivityResult.contains(ConnectivityResult.satellite)) {
+      // Carrier-provided satellite network available
+      _showToast(context, "Satellite network available.");
+    } else if (connectivityResult.contains(ConnectivityResult.other)) {
+      // Connected to a network which is not in the above mentioned networks.
+      _showToast(context, "Other network available.");
+    } else if (connectivityResult.contains(ConnectivityResult.none)) {
+      // No available network types
+      setState(() {
+        _showAlertDialog(context, "No Internet", "Please check your internet.");
+      });
+    }
   }
 
   Widget build(BuildContext context) {
@@ -69,4 +113,68 @@ class SecondScreen extends StatelessWidget {
       ),
     );
   }
+}
+
+// Step 4: Show toast message
+void _timer(BuildContext context) {
+  // เมื่อครบ 3 วิ ให้ไปหน้า Second Screen
+  Timer(
+    const Duration(seconds: 3),
+    () => Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SecondScreen()),
+    ),
+  );
+}
+
+void _showToast(BuildContext context, String msg) {
+  Fluttertoast.showToast(
+    msg: msg,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 1,
+    backgroundColor: Colors.brown,
+    textColor: Colors.white,
+    fontSize: 24,
+  );
+  _timer(context);
+}
+
+void _showAlertDialog(BuildContext context, String title, String msg) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(
+          title,
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.redAccent,
+            fontWeight: FontWeight.w500,
+            fontFamily: "Alike",
+          ),
+        ),
+        content: Text(msg),
+        actions: <Widget>[
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.black45),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text(
+              "OK",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.w500,
+                fontFamily: "Alike",
+              ),
+            ),
+          ),
+        ],
+      );
+    },
+  );
 }
